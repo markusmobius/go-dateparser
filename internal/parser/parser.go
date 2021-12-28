615d034f042269879f31e0369e3ed199a4241b4c
Radhi Fadlillah <m.radhi.f@gmail.com>
2021-12-28 14:06:11 +0700

    Implement time parser

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,7 +1,9 @@
 package parser
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/markusmobius/go-dateparser/internal/setting"
@@ -64,4 +66,31 @@ func parseWithFormats(cfg *setting.Configuration, str string, formats ...string)
 	}
 
 	return DateData{Period: period}
+}
+
+func parseTime(s string) (time.Time, error) {
+	timeFormats := []string{
+		"15:04:05",
+		"3:04:05 PM",
+		"15:04",
+		"3:04 PM",
+		"3 PM",
+		"15:04:05.999999",
+		"3:04:05.999999 PM",
+		"15:04 PM",
+	}
+
+	tmp := strings.TrimSpace(s)
+	tmp = strings.ToUpper(tmp)
+	for _, format := range timeFormats {
+		t, err := time.Parse(format, tmp)
+		if err != nil {
+			continue
+		}
+
+		return t, nil
+	}
+
+	err := fmt.Errorf("%s doesn't seem to be a valid time", s)
+	return time.Time{}, err
 }
--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -85,3 +85,40 @@ func Test_parseWithFormats(t *testing.T) {
 	dateAsExpected(dt, "2014-08-04")
 
 }
+
+func Test_parseTime(t *testing.T) {
+	// Helper function
+	isEqual := func(s string, hour, min, sec, msec int) {
+		expected := time.Date(0, 1, 1, hour, min, sec, msec*1_000, time.UTC)
+		result, _ := parseTime(s)
+		assert.Equal(t, expected, result, s)
+	}
+
+	isZero := func(s string) {
+		result, _ := parseTime(s)
+		assert.True(t, result.IsZero(), s)
+	}
+
+	// Time should be parsed
+	isEqual("11:30:14", 11, 30, 14, 0)
+	isEqual("11:30", 11, 30, 0, 0)
+	isEqual("11:30 PM", 23, 30, 0, 0)
+	isEqual("13:30 PM", 13, 30, 0, 0)
+	isEqual("16:14 AM", 16, 14, 0, 0)
+	isEqual("23:30 AM", 23, 30, 0, 0)
+	isEqual("1:30 AM", 1, 30, 0, 0)
+	isEqual("1:30:15.330 AM", 1, 30, 15, 330000)
+	isEqual("1:30:15.330 PM", 13, 30, 15, 330000)
+	isEqual("1:30:15.3301 PM", 13, 30, 15, 330100)
+	isEqual("11:20:05 AM", 11, 20, 5, 0)
+	isEqual("14:30:15.330100", 14, 30, 15, 330100)
+
+	// Time is invalid
+	isZero("11")
+	isZero("22:12:12 PM")
+	isZero("22:12:10:16")
+	isZero("10:14.123 PM")
+	isZero("2:13:88")
+	isZero("23:01:56.34 PM")
+	isZero("2.45 PM")
+}
